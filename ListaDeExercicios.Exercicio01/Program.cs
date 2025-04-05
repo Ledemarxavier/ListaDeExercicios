3ccba42fd2a50a64598468c91f55ea34c418a47f
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-05 13:06:08 -0300

    Add project files.

---
+++ ListaDeExercicios.Exercicio01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Dynamic;
+
+namespace ListaDeExercicios.Exercicio01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("** Calculo do Volume de uma caixa **");
+
+                Console.WriteLine("Digite a altura da caixa: ");
+                decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Digite a largura da caixa: ");
+                decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Digite o comprimento da caixa: ");
+                decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+                decimal volume = comprimento * largura * altura;
+
+                Console.WriteLine($"O volume da caixa é de: {volume}m³");
+
+                Console.Write("\nDeseja continuar? (S/N)");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
