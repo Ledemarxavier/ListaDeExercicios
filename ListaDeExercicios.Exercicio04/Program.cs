f41287870bfa064d4e8eba4021befd3be180f0b2
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-05 19:22:13 -0300

    exercício 04

---
+++ ListaDeExercicios.Exercicio04/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ListaDeExercicios.Exercicio04
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("*** Conversor de temperatura ***");
+
+            Console.Write("\nDigite a temperatura em Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine($"\n{celsius}°C equivale a {fahrenheit}°F");
+
+            Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
