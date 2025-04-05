671e1b5e2b253d63f67df20a86bd2ee3cc11b04b
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-05 19:14:47 -0300

    exercicio 03

---
+++ ListaDeExercicios.Exercicio03/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ListaDeExercicios.Exercicio03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("**** Calculadora de consumo Km/l ****");
+
+                Console.WriteLine("Digite a quilometragem inicial: ");
+                double kmInicial = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a quilometragem final: ");
+                double kmFinal = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a quantidade de combustível consumida (litros): ");
+                double combustivelConsumido = Convert.ToDouble(Console.ReadLine());
+
+                double distanciaPercorrida = kmFinal - kmInicial;
+
+                double consumoPorKm = distanciaPercorrida / combustivelConsumido;
+
+                Console.WriteLine($"\nDistância percorrida: {distanciaPercorrida} km");
+                Console.WriteLine($"Consumo médio: {consumoPorKm:F2} km/l");
+                break;
+
+                Console.Write("\nDeseja continuar? (S/N)");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
