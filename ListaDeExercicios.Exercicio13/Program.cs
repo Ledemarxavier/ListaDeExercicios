fd766ce1f3218ac35d8f4095ebab299d30126245
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-06 23:40:41 -0300

    exercício 13

---
+++ ListaDeExercicios.Exercicio13/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ListaDeExercicios.Exercicio13
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                //Calculo de fatorial de um número
+                Console.Clear();
+                Console.WriteLine("Digite um número para calcular o fatorial: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int fatorial = 1;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    fatorial *= i;
+                }
+
+                Console.WriteLine($"Fatorial de, {n} é {fatorial}");
+
+                Console.Write("\nDeseja calcular novamente? (S/N): ");
+                string opcao = Console.ReadLine().ToUpper();
+
+                if (opcao != "S")
+                    break;
+            }
+        }
+    }
+}
