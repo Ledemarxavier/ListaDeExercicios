b4a19322da813d48aaa0841cffb661b1016f2267
Ledemarxavier <ledemarxdeoliveira@gmail.com>
2025-04-06 23:09:33 -0300

    exercício 12

---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        // Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Números ímpares entre 100 e 200:\n");
+            for (int i = 101; i < 200; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Aperte ENTER para sair...");
+            Console.ReadKey();
+        }
+    }
+}
